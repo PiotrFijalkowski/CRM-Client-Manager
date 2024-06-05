bca21e33e1c064af918a43a24933494282c802e7
PiotrFijalkowski <seejay12@gmail.com>
2024-06-06 00:23:40 +0200

    add firebase + change css to sass

---
+++ src/firebase/config.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCO-Gydmf8iYswsJsAo-el750WvY6OGSJ8",
+  authDomain: "punkt-deweloperski.firebaseapp.com",
+  projectId: "punkt-deweloperski",
+  storageBucket: "punkt-deweloperski.appspot.com",
+  messagingSenderId: "866858871639",
+  appId: "1:866858871639:web:c8049790515ec6942f2c12",
+  measurementId: "G-S7CVBLH146"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
